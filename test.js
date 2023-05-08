8513d45826aa0aceafb73d44b5b88317e86e8b02
whoavesh <127306466+whoavesh@users.noreply.github.com>
2023-05-08 17:43:31 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // Requiring module
-const asert = require('assert');
+const assert = require('assert');
 
 // We can group similar tests inside a describe block
 describe("Simple Calculations", () => {
